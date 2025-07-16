f25488bdb66d9a0d71c0df04c3256c9dd7d25aab
abhay-singh-shekhawat <shekhawaabhay80@gmail.com>
2025-07-16 10:08:58 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Simple scroll animation on load
-window.addEventListener("load", () => {
-  document.querySelector(".hero").style.opacity = "1";
-  document.querySelector(".hero").style.transform = "translateY(0)";
-});
