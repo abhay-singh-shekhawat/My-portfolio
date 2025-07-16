f9606b41be7c3f62e6dd1f28e4a64b6a002f65da
abhay-singh-shekhawat <shekhawaabhay80@gmail.com>
2025-07-16 10:03:11 +0530

    Add files via upload
    this is my 1st portfolio made by me for the internship i completed by organized by learn and build

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple scroll animation on load
+window.addEventListener("load", () => {
+  document.querySelector(".hero").style.opacity = "1";
+  document.querySelector(".hero").style.transform = "translateY(0)";
+});
